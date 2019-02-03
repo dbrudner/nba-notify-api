1fbf4eb4f10951fbc7e0c686431c29af74a287c5
unknown <dbrudne1@gmail.com>
2019-02-03 16:29:30 -0600

    changing to mlab

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ mongoose.connection.on("open", function() {
 	console.log("Connected to mongo server.");
 });
 
-mongoose.connect(process.env.ATLAS_URI, { useNewUrlParser: true });
+mongoose.connect(process.env.MONGO_URI, { useNewUrlParser: true });
 
 const server = express();
 
