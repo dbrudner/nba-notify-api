e64acdf1298f88033f9204260eb824080a3c91b8
unknown <dbrudne1@gmail.com>
2019-02-03 16:18:25 -0600

    dotenv for production

--- server.js
+++ server.js
@@ -1,4 +1,4 @@
-require("dotenv").config();
+if (process.env.NODE_ENV !== "production") require("dotenv").config();
 const bodyParser = require("body-parser");
 const cors = require("cors");
 const express = require("express");
